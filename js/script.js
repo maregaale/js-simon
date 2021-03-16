877ec95e012908051a51dffda6d3256d2ccbdca6
Alessandro Marega <ale_marega@hotmail.it>
2021-03-16 15:00:00 +0100

    inizio progetto e ggiunto prima funzione

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Un alert() espone 5 numeri generati casualmente.
+// Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// variabili
+
+
+// funzioni
+// funzione che determina un numero random
+function numRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
