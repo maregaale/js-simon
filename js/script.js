dd68923bab9e112d77f1764d1288e1dcdb7eb897
Alessandro Marega <ale_marega@hotmail.it>
2021-03-16 15:18:09 +0100

    aggiunto countdown in console

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-// Da li parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 // variabili
-
+var numeriRandom = "";
 
 // funzioni
 // funzione che determina un numero random
@@ -14,8 +13,6 @@ function numRandom(min, max) {
 
 // 1. genero un alert con 5 numeri random da 1 a 100
 // genero 5 numeri numeri random e li inserisco in un array
-var numeriRandom = "";
-
 for (var i = 0; i < 5; i++) {
   var numero = numRandom(1, 100);
   if (numeriRandom.includes(numero) == false) {
@@ -23,4 +20,18 @@ for (var i = 0; i < 5; i++) {
   }
 }
 
+// genero l'alert
 alert("i numeri da ricordare sono:  " + numeriRandom);
+
+// 2. faccio partire il timer di 30 secondi
+var numCountdown = 30;
+
+var countdown = setInterval(function () {
+  console.log(numCountdown);
+
+  if (numCountdown == 0) {
+    clearInterval(countdown);
+  }
+
+  numCountdown -= 1;
+}, 1000);
