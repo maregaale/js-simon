8a4736d06aa2db2100184a6ca1b60ad7092b712f
Alessandro Marega <ale_marega@hotmail.it>
2021-03-16 15:39:43 +0100

    fix sintassi script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 // variabili
-var numeriRandom = "";
+var numeriRandom = [];
+var numeriUtente = "";
+var risultato = "i numeri da ricordare sono: " + " ";
 
 // funzioni
 // funzione che determina un numero random
@@ -16,12 +18,20 @@ function numRandom(min, max) {
 for (var i = 0; i < 5; i++) {
   var numero = numRandom(1, 100);
   if (numeriRandom.includes(numero) == false) {
-    numeriRandom += numero + " " + " ";
+    numeriRandom[i] = numero;
+    if (i != 4) {
+      risultato += numeriRandom[i] + ", " + " ";
+    } else {
+      risultato += numeriRandom[i];
+    }
+  } else {
+    i--;
   }
 }
 
+
 // genero l'alert
-alert("i numeri da ricordare sono:  " + numeriRandom);
+alert(risultato);
 
 // 2. faccio partire il timer di 30 secondi
 var numCountdown = 30;
@@ -34,4 +44,7 @@ var countdown = setInterval(function () {
   }
 
   numCountdown -= 1;
+
+  // 3. l'utente dopo i 30 secondi inserisce per 5 volte un numero
+
 }, 1000);
