ace0558b7e666f7880320f720ed06faae4841e31
Alessandro Marega <ale_marega@hotmail.it>
2021-03-16 16:24:19 +0100

    aggiunto controllo sui numeri inseriti e stampa risultato

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-
 // variabili
 var numeriRandom = [];
 var numeriUtente = "";
 var risultato = "i numeri da ricordare sono: " + " ";
+var numeriUtente = [];
+var numOk = [];
+var numCountdown = 30;
 
 // funzioni
 // funzione che determina un numero random
@@ -28,27 +29,34 @@ for (var i = 0; i < 5; i++) {
   }
 }
 
-
 // genero l'alert
 alert(risultato);
 
 // 2. faccio partire il timer di 30 secondi
-var numCountdown = 30;
-
 var countdown = setInterval(function () {
   console.log(numCountdown);
 
   if (numCountdown == 0) {
+
     clearInterval(countdown);
+
     // 3. l'utente dopo i 30 secondi inserisce per 5 volte un numero
-    var numeriUtente = [];
     for (var j = 0; j < 5; j++) {
       numeriUtente[j] = parseInt(prompt("inserisci il " + (j + 1) + "° " + "numero"));
     }
-    console.log(numeriUtente);
+
+    // 4. controllo se tra i numeri inseriti dall'utente ce n'è uno di quelli nell'alert
+    for (var k = 0; k < numeriUtente.length; k++) {
+      if (numeriRandom.includes(numeriUtente[k]) == true) {
+        numOk.push(numeriUtente[k]);
+      }
+    }
+
+    // 5. stampo il risultato in quantità e qualità dei numeri ricordati
+    console.log("hai ricordato " + numOk.length + " numeri");
+    console.log("i numeri da te ricordati sono: " + numOk);
   }
 
   numCountdown -= 1;
-
 
 }, 1000);
